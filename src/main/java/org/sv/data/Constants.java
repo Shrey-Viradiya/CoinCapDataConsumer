ab16a160b14b2b8efaacbd9791b4319c30a309e7
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2024-05-18 11:31:31 +0530

    Adding Ethereum

--- src/main/java/org/sv/data/Constants.java
+++ src/main/java/org/sv/data/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String ASSETS_DATA_ENDPOINT = "/v2/assets";
     public static final String RATES_DATA_ENDPOINT = "/v2/rates";
     public static final String MARKETS_DATA_ENDPOINT = "/v2/markets";
-    public static final String PRICES_DATA_WEBSOCKET_URL = "wss://ws.coincap.io/prices?assets=bitcoin";
+    public static final String PRICES_DATA_WEBSOCKET_URL = "wss://ws.coincap.io/prices?assets=bitcoin,ethereum";
     public static final String TRADES_DATA_WEBSOCKET_URL = "wss://www.nseindia.com/api/quote-equity?symbol=SBIN";
     public static final List<String> DATA_ELIGIBLE_EXCHANGES = FastList.newListWith("binance");
 }
